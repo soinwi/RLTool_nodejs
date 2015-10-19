c7dd066fd73b9518617b77eb90467b2d1516d02f
soinwi <ds@soinwi.ch>
2015-10-19 09:02:41 +0000

    changed odata-framework now using simple-odata-server instead of node-odata old framework did not support , which must be provided in order to be used with openui5

--- resources/people.js
+++ resources/people.js
@@ -1,11 +1,18 @@
-var Resource = require('node-odata').Resource;
 
-module.exports = Resource('people', {
-    firstName: String,
-    lastName: String,
-    birthDate: Date,
-    _id: {
-        type: String,
-        select: false
+module.exports = {
+    namespace: "people",
+    entityTypes:{
+        "PersonType": {
+            "_id": {"type": "Edm.String", key: true},
+            "firstName": {"type": "Edm.String"},
+            "lastName": {"type": "Edm.String"},
+            "birtDate": {"type": "Edm.Date"}
+        }
+        
+    },
+    entitySets:{
+        "people": {
+            entityType: "people.PersonType"
+        }
     }
-});
+};
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-var odata = require('node-odata');
+var express = require('express');
+var app = express();
+var ODataServer = require('simple-odata-server');
+var MongoClient = require('mongodb').MongoClient;
 
 
-var server = odata('mongodb://localhost:27017/test');
+MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+    var odataServer = new ODataServer()
+        .model(require('./resources/people'))
+        .onMongo(function(cb) {
+            cb(err, db);
+        });
 
-server.use(require('./resources/people'));
-server.use(require('./resources/results'));
-server.use(require('./resources/static'));
+    app.use('/odata', odataServer.handle.bind(odataServer));
+    app.use(require('./resources/static'));
+    app.listen(process.env.PORT || 3000, function() {
+        console.log("Server running...")
+    });
+});
 
 
 
-server.listen(process.env.PORT || 3000, function(){console.log("Server running...")});
+// var odata = require('node-odata');
+
+
+// var server = odata('mongodb://localhost:27017/test');
+
+// server.use(require('./resources/people'));
+// server.use(require('./resources/results'));
+// server.use(require('./resources/static'));
+
+
+
+// server.listen(process.env.PORT || 3000, function(){console.log("Server running...")});
