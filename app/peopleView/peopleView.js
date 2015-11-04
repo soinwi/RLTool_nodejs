057a2ef1509c666c0a6eb7c39ceaec3f53e0458e
soinwi <ds@soinwi.ch>
2015-11-04 07:52:22 +0000

    can create or delete entries now

--- app/peopleView/peopleView.js
+++ app/peopleView/peopleView.js
@@ -1,33 +1,52 @@
 angular.module('rltoolApp.peopleView', ['ngRoute', 'ODataResources'])
 
-.config(['$routeProvider', function($routeProvider)
-{
-    $routeProvider.when('/people',
-        {
-            templateUrl: 'peopleView/peopleView.html',
-            controller: 'peopleView.peopleViewController'
-        });
-    
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/people', {
+        templateUrl: 'peopleView/peopleView.html',
+        controller: 'peopleView.peopleViewController'
+    });
+
 }])
 
-.controller('peopleView.peopleViewController', ['$scope', '$odataresource', function($scope, $odataresource){
-    
-    var people = $odataresource('/odata/people/');
-    $scope.people = people.odata().query(
-        function(){
-        },
-        function(e){
-            alert("error getting people data from /odata/people/");
-        }
-        
+.controller('peopleView.peopleViewController', ['$scope', '$odataresource', function($scope, $odataresource) {
+
+
+
+    var peopleResource = $odataresource('/odata/people/', {}, {}, {
+        odatakey: '_id'
+    });
+
+    var updatePeopleList = function() {
+        $scope.people = peopleResource.odata().query(
+            function() {},
+            function(e) {
+                alert("error getting people data from /odata/people/");
+            }
+
         );
+    };
+    updatePeopleList();
+
+    $scope.detailView = function(person) {
+        alert("person clicked: " + person.firstName);
+    };
+
+    $scope.deletePerson = function(person) {
+        person.$delete();
+        updatePeopleList();
+    };
+
+    $scope.addNew = function() {
+
+        var p = new peopleResource();
+
+        p.firstName = "max";
+        p.lastName = "mustermann";
+
+
+        p.$save();
         
-        $scope.detailView = function(person){
-          alert("person clicked: " + person.firstName);  
-        };
-        
-        $scope.addNew = function(){
-          alert("addnew clicked");  
-        };
-    
+        updatePeopleList();
+    };
+
 }]);
